2a1fbd1da71d3ecb6df356df71e38e5362ee3417
Andrew Menning <menning23@gmail.com>
2016-07-29 17:21:02 -0400

    Remove legacy code and update var names

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -4,32 +4,26 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Goutte\Client;
 
 class MainController extends Controller
 {
     public function indexAction(Request $request)
     {
         $session = $request->getSession();
-        $user_id = $session->get('user_id');
-        if (isset($user_id) && !empty($user_id)) {
+        $userID = $session->get('user_id');
+        if (isset($userID) && !empty($userID)) {
             $firstname = $session->get('firstname');
             $lastname = $session->get('lastname');
         } else {
-            $user_id = -1;
+            $userID = -1;
             $firstname = null;
             $lastname = null;
         }
 
         return $this->render('main/symfonyStocks.html.twig', array(
-            'user_id' => $user_id,
+            'user_id' => $userID,
             'firstname' => $firstname,
             'lastname' => $lastname,
         ));
-    }
-
-    public function showRulesAction()
-    {
-        return $this->render('rules/rules.html.twig', array());
     }
 }
