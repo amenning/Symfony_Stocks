db41d05d0d14041619bd4cc005b757ba2d0806f3
Andrew Menning <menning23@gmail.com>
2016-07-28 23:31:16 -0400

    clean up stock ctrl

--- src/AppBundle/Controller/StockController.php
+++ src/AppBundle/Controller/StockController.php
@@ -8,26 +8,6 @@ use Goutte\Client;
 
 class StockController extends Controller
 {
-    public function indexAction(Request $request)
-    {
-        $session = $request->getSession();
-        $user_id = $session->get('user_id');
-        if (isset($user_id) && !empty($user_id)) {
-            $firstname = $session->get('firstname');
-            $lastname = $session->get('lastname');
-        } else {
-            $user_id = -1;
-            $firstname = null;
-            $lastname = null;
-        }
-
-        return $this->render('main/symfonyStocks.html.twig', array(
-            'user_id' => $user_id,
-            'firstname' => $firstname,
-            'lastname' => $lastname,
-        ));
-    }
-
     public function stockAction(Request $request, $ticker)
     {
 
